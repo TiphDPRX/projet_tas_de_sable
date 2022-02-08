a8ebc873ea6b5fc85ca6d0c1c68878b43141ffa8
Tiphanie DEPREAUX <tiphanie.depreaux@ens.uvsq.fr>
2022-02-08 12:17:41 +0100

    dernière modfification

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -6,4 +6,19 @@
 # https://github.com/uvsq22102500/projet_tas_de_sable
 ###########################
 
-print("test")
+############### Constantes
+HEIGHT = 500
+WIDTH = 500
+
+########### Partie Principale
+import tkinter as tk
+from tkinter.messagebox import YES
+
+print("hello world")
+racine = tk.Tk()
+racine.title("Tas de Sable")
+racine.geometry("600x600")
+canvas = tk.Canvas(racine, height = HEIGHT, width = WIDTH)
+canvas.pack(expand = YES)
+racine.mainloop()
+
