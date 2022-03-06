7a90a8303107545bf18699f2afe00ac92cb34ba1
Ania AOUAOUCHE <ania.aouaouche@ens.uvsq.fr>
2022-03-07 00:35:12 +0100

    a

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -93,36 +93,8 @@ boutton_sou=Button(window,command=soustraction)
 
 '''faire appel a config_courante'''
 window.mainloop()
-=======
-############### 
-# Librairies
-import tkinter as tk
-
-############### 
-# Constantes
-HEIGHT = 500
-WIDTH = 500
-N = 3
-
-############## 
-# Fonctions
-def grille():
-    grille = []
-    for i in range(N):
-        grille.append([0]*N)
-    print(grille)
 
 
-############
-# Partie Principale
 
-racine = tk.Tk()
-racine.title("Tas de Sable")
-racine.geometry("600x600")
-canvas = tk.Canvas(racine, height = HEIGHT, width = WIDTH)
-canvas.pack()
-
-
-racine.mainloop()
 
 
